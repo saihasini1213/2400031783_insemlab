71a086913a0e2e0c5c7b2870b54e2ddf55abe82c
saihasini1213 <2400031783@kluniversity.in>
2025-10-15 20:35:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Courses from "./components/Courses";
-import Contact from "./components/Contact";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import Home from "./Home.jsx";
+import Courses from "./Courses.jsx";
+import Contact from "./Contact.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="app-center-container">
+        <nav className="navbar">
+          <ul>
+            <li>
+              <NavLink to="/" end className={({ isActive }) => (isActive ? "active" : "")}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/courses" className={({ isActive }) => (isActive ? "active" : "")}>
+                Courses
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className={({ isActive }) => (isActive ? "active" : "")}>
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="centered-content">
+          <div className="page-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/courses" element={<Courses />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
-export default App;
 
+export default App;
