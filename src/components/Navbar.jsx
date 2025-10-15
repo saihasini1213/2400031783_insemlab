19a942be5f0ab7813025bb16fc37f8572739ea88
saihasini1213 <2400031783@kluniversity.in>
2025-10-15 20:34:55 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,1 @@
-import { NavLink } from "react-router-dom";
-import "./Navbar.css";
 
-function Navbar() {
-  return (
-    <nav>
-      <NavLink to="/" className={({isActive}) => isActive ? "active" : ""}>Home</NavLink>
-      <NavLink to="/courses" className={({isActive}) => isActive ? "active" : ""}>Courses</NavLink>
-      <NavLink to="/contact" className={({isActive}) => isActive ? "active" : ""}>Contact</NavLink>
-    </nav>
-  );
-}
-
-export default Navbar;
